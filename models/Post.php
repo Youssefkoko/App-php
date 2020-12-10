7b3a8fa05da84f8184a9689dd183bf7d6fb9cf29
Youssef Khoukhou <youssefkoko01@gmail.com>
2020-12-10 23:53:15 +0100

    create Post model

--- models/Post.php
+++ models/Post.php
@@ -1,1 +1,18 @@
-<php
+<?php
+
+class Post extends Database
+{
+  private $db;
+  public function __construct()
+
+  {
+    $this->db = new Database();
+  }
+
+  public function getPosts()
+  {
+    $this->db->query('SELECT * FROM posts');
+    $results  = $this->db->resultSet();
+    return $results;
+  }
+}
